39cae40cf48183c6d2550317e920a84cdcc79f0d
poblouin <pblouin@wealthsimple.com>
2023-12-02 10:14:04 -0500

    day02 part 2

--- src/day02/Solution.kt
+++ src/day02/Solution.kt
@@ -1,25 +1,30 @@
 package day02
 
 import readInput
-import readInputLines
 
-data class Game(val id: Int, val rawRounds: List<String> = emptyList()) {
+data class Game(val id: Int) {
     companion object {
         const val MAX_RED = 12
         const val MAX_GREEN = 13
         const val MAX_BLUE = 14
     }
 
-    val rounds: List<Round> = buildRounds()
+    private var rounds: List<Round> = emptyList()
 
     fun withinRules(): Boolean {
-        return rounds.all { round ->
-            round.blue <= MAX_BLUE && round.red <= MAX_RED && round.green <= MAX_GREEN
-        }
+        return rounds.all { it.blue <= MAX_BLUE && it.red <= MAX_RED && it.green <= MAX_GREEN }
     }
 
-    private fun buildRounds(): List<Round> {
-        return rawRounds.map {
+    fun findMaxEachColorAcrossRounds(): List<Int> {
+        return listOf(
+            rounds.maxOf { it.blue },
+            rounds.maxOf { it.red },
+            rounds.maxOf { it.green },
+        )
+    }
+
+    fun buildRounds(rawRounds: List<String>): Game {
+        rounds = rawRounds.map {
             val pairs = "(\\d+)\\s(\\w+)".toRegex()
                 .findAll(it)
                 .map { it.groupValues[2] to it.groupValues[1].toInt() }.toList()
@@ -27,30 +32,30 @@ data class Game(val id: Int, val rawRounds: List<String> = emptyList()) {
             Round(
                 blue = pairs.find { it.first == "blue" }?.second ?: 0,
                 red = pairs.find { it.first == "red" }?.second ?: 0,
-                green = pairs.find { it.first == "green" }?.second ?: 0
+                green = pairs.find { it.first == "green" }?.second ?: 0,
             )
         }
+
+        return this
     }
 }
 
-data class Round(var blue: Int = 0, var red: Int = 0, var green: Int = 0) {
-}
+data class Round(var blue: Int = 0, var red: Int = 0, var green: Int = 0)
 
 fun part1(input: List<Game>): Int {
     return input.filter { it.withinRules() }.sumOf { it.id }
 }
 
-fun part2(input: List<String>): List<String> {
-    return input
+fun part2(input: List<Game>): Int {
+    return input.sumOf { it.findMaxEachColorAcrossRounds().reduce { acc, num -> acc * num } }
 }
 
 fun main(args: Array<String>) {
     val input = readInput(args[0])
         .split("\n")
         .map { it.split(":") }
-        .map { Game("\\d+".toRegex().find(it[0])!!.value.toInt(), it[1].split(";")) }
-    println(part1(input))
-//    println(part2(input))
+        .map { Game("\\d+".toRegex().find(it[0])!!.value.toInt()).buildRounds(it[1].split(";")) }
+
     check(part1(input) == 2256)
-//    check(part2(input) == 52834)
+    check(part2(input) == 74229)
 }
